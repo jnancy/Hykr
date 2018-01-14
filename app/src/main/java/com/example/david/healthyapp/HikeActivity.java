08d2486aebbad7607cdadbe10f9472f60638acdd
david <davidlouie45@gmail.com>
2018-01-13 19:37:36 -0800

    Added navigation bar

---
+++ app/src/main/java/com/example/david/healthyapp/HikeActivity.java
@@ -0,0 +1,15 @@
+package com.example.david.healthyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class HikeActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame); //Remember this is the FrameLayout area within your activity_main.xml
+        getLayoutInflater().inflate(R.layout.activity_hike, contentFrameLayout);
+    }
+}
